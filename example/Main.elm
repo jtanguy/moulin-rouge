048e003f29e23038bd805bc125aecc50671eaa24
Julien Tanguy <julien.tanguy@jhome.fr>
2017-03-12 22:37:42 +0100

    docs: Update the main example

--- example/Main.elm
+++ example/Main.elm
@@ -6,75 +6,11 @@ import Keyboard
 import Slides exposing (slide)
 
 
-type alias Model =
-    { state : Slides.State
-    }
-
-
-type Msg
-    = SlideMsg Slides.Msg
-    | NoOp
-
-
-initial : Model
-initial =
-    { state = Slides.beginning
-    }
-
-
-keyEvents : Sub Msg
-keyEvents =
-    let
-        decider code =
-            case fromCode code of
-                Right ->
-                    SlideMsg Slides.Next
-
-                Left ->
-                    SlideMsg Slides.Prev
-
-                Home ->
-                    SlideMsg Slides.Begin
-
-                End ->
-                    SlideMsg Slides.End
-
-                _ ->
-                    NoOp
-    in
-        Keyboard.downs decider
-
-
 main =
-    program
-        { init = initial ! []
-        , subscriptions = \_ -> keyEvents
-        , view = view
-        , update = myupdate
-        }
-
-
-myupdate msg model =
-    case msg of
-        SlideMsg j ->
-            { model | state = update j model.state } ! []
-
-        NoOp ->
-            model ! []
-
-
-{ config, update } =
-    Slides.config
-        { slides =
-            [ slide [] [ h1 [] [ text (toString 0) ] ]
-            , slide [] [ h1 [] [ text (toString 1) ] ]
-            , slide [] [ h1 [] [ text (toString 2) ] ]
-            , slide [] [ h1 [] [ text (toString 3) ] ]
-            , slide [] [ h1 [] [ text (toString 4) ] ]
-            ]
-        }
-
-
-view : Model -> Html Msg
-view model =
-    Slides.view config model.state
+    Slides.basicProgram
+        [ slide [] [ h1 [] [ text (toString 0) ] ]
+        , slide [] [ h1 [] [ text (toString 1) ] ]
+        , slide [] [ h1 [] [ text (toString 2) ] ]
+        , slide [] [ h1 [] [ text (toString 3) ] ]
+        , slide [] [ h1 [] [ text (toString 4) ] ]
+        ]
